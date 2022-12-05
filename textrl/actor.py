ca9459304d23d51ae6a6aa26fc4331ef88710e45
voidful <voidful.stack@gmail.com>
2022-12-05 17:11:40 +0800

    re-implement actor and env

--- textrl/actor.py
+++ textrl/actor.py
@@ -2,6 +2,8 @@ import copy
 
 import pfrl
 import torch
+import torch.nn.functional as F
+from pfrl.agents.ppo import _elementwise_clip
 
 
 class TextRLActor:
@@ -18,19 +20,14 @@ class TextRLActor:
     def agent_ppo(self, update_interval=10, minibatch_size=3000, epochs=20):
         policy = torch.nn.Sequential(
             self.converter,
-            pfrl.policies.GaussianHeadWithStateIndependentCovariance(
-                action_size=self.n_actions,
-                var_type="diagonal",
-                var_func=lambda x: torch.exp(2 * x),  # Parameterize log std
-                var_param_init=0,  # log std = 0 => std = 1
-            ),
+            SoftmaxCategoricalHead()
         )
         vf = torch.nn.Sequential(
             torch.nn.Linear(self.obs_size, 1),
         )
         model = pfrl.nn.Branched(policy, vf)
         opt = torch.optim.Adam(model.parameters(), lr=3e-4)
-        agent = pfrl.agents.PPO(
+        agent = TextPPO(
             model,
             opt,
             gpu=self.device,
@@ -39,18 +36,18 @@ class TextRLActor:
             epochs=epochs,
             clip_eps_vf=None,
             entropy_coef=0,
+            gamma=1,
+            lambd=1,
             standardize_advantages=True,
-            gamma=0.995,
-            lambd=0.97,
             act_deterministically=True
         )
         self.agent = agent
         return agent
 
-    def predict(self, input_text, max_episode_len=100):
+    def predict(self, input_item, max_episode_len=100):
         t = 0
         with self.agent.eval_mode():
-            obs = self.env.reset(input_text)
+            obs = self.env.reset(input_item)
             while True:
                 action = self.agent.act(obs)
                 obs, reward, done, pred = self.env.step(action)
@@ -59,3 +56,49 @@ class TextRLActor:
                 self.agent.observe(obs, reward, done, reset)
                 if done or reset:
                     return pred.get('predicted_str')
+
+
+class TextPPO(pfrl.agents.PPO):
+    def _lossfun(
+            self, entropy, vs_pred, log_probs, vs_pred_old, log_probs_old, advs, vs_teacher
+    ):
+
+        prob_ratio = torch.exp(log_probs - log_probs_old)
+        loss_policy = -torch.mean(
+            torch.min(
+                (prob_ratio.T * advs).T,
+                (torch.clamp(prob_ratio, 1 - self.clip_eps, 1 + self.clip_eps).T * advs).T,
+            ),
+        )
+
+        if self.clip_eps_vf is None:
+            loss_value_func = F.mse_loss(vs_pred, vs_teacher)
+        else:
+            clipped_vs_pred = _elementwise_clip(
+                vs_pred,
+                vs_pred_old - self.clip_eps_vf,
+                vs_pred_old + self.clip_eps_vf,
+            )
+            loss_value_func = torch.mean(
+                torch.max(
+                    F.mse_loss(vs_pred, vs_teacher, reduction="none"),
+                    F.mse_loss(clipped_vs_pred, vs_teacher, reduction="none"),
+                )
+            )
+        loss_entropy = -torch.mean(entropy)
+
+        self.value_loss_record.append(float(loss_value_func))
+        self.policy_loss_record.append(float(loss_policy))
+
+        loss = (
+                loss_policy
+                + self.value_func_coef * loss_value_func
+                + self.entropy_coef * loss_entropy
+        )
+
+        return loss
+
+
+class SoftmaxCategoricalHead(torch.nn.Module):
+    def forward(self, logits, temperature=1):
+        return torch.distributions.Categorical(logits=logits / temperature)
--- textrl/environment.py
+++ textrl/environment.py
@@ -1,8 +1,8 @@
 import random
 
 import gym
-import torch
 import numpy
+import torch
 
 
 class TextRLEnv(gym.Env):
@@ -14,7 +14,7 @@ class TextRLEnv(gym.Env):
         self.tokenizer = tokenizer
         self.observation_space = observation_input
         self.target_table = {}
-        self.input_text = ""
+        self.input_item = [""]
         self.predicted = []
 
         self.reset()
@@ -23,27 +23,32 @@ class TextRLEnv(gym.Env):
         if isinstance(action, numpy.ndarray):
             action = numpy.argmax(action)
         predicted, finish, predicted_str = self._predict(vocab_id=action)
-        reward = self.get_reward(self.input_text, predicted, finish)
+        reward = self.get_reward(self.input_item, predicted, finish)
         self.predicted = predicted
         return self._get_obs(predicted), reward, finish, {"predicted_str": predicted_str}
 
-    def get_reward(self, input_text, predicted_list, finish):
+    def get_reward(self, input_item, predicted_list, finish):
         reward = 1
         return reward
 
-    def reset(self, input_text=None):
+    def gat_obs_input(self, input_item):
+        return input_item[0]
+
+    def reset(self, input_item=None):
         self.predicted = []
-        if input_text is None:
-            self.input_text = random.choice(self.observation_space)
+        if input_item is None:
+            self.input_item = random.choice(self.observation_space)
         else:
-            self.input_text = input_text
+            self.input_item = input_item
         return self._get_obs()
 
     def _get_obs(self, predicted=[]):
         with torch.no_grad():
             p_text = self.tokenizer.convert_tokens_to_string(predicted)
             if len([k for k, v in self.model.named_parameters() if 'decoder' in k]) > 0:
-                feature_dict = self.tokenizer([self.input_text], return_tensors='pt', add_special_tokens=False).to(
+                feature_dict = self.tokenizer([self.gat_obs_input(self.input_item)],
+                                              return_tensors='pt',
+                                              add_special_tokens=False).to(
                     self.model.device)
                 predicted = [self.tokenizer.eos_token] + predicted
                 dec_input = torch.tensor([self.tokenizer.convert_tokens_to_ids(predicted)]).to(self.model.device)
@@ -51,7 +56,8 @@ class TextRLEnv(gym.Env):
                 prediction = self.model(**feature_dict, output_hidden_states=True)
                 outputs = prediction.decoder_hidden_states[-1].squeeze(0)
             else:
-                feature_dict = self.tokenizer([[self.input_text, p_text]], return_tensors='pt',
+                feature_dict = self.tokenizer([[self.gat_obs_input(self.input_item), p_text]],
+                                              return_tensors='pt',
                                               add_special_tokens=False).to(self.model.device)
                 prediction = self.model(**feature_dict, output_hidden_states=True)
                 outputs = prediction.hidden_states[-1].squeeze(0)
