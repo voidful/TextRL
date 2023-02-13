2e41aa3366222c78e7ffef531e00436104fc9075
voidful <voidful.stack@gmail.com>
2023-02-14 03:46:15 +0800

    bump version

--- textrl/__init__.py
+++ textrl/__init__.py
@@ -1,3 +1,4 @@
-from textrl.environment import TextRLEnv
 from textrl.actor import TextRLActor
+from textrl.environment import TextRLEnv
+from textrl.evaluator import Evaluator, save_agent
 from textrl.trainer import train_agent_with_evaluation
